5432b691513ca104455b631b1d43e74707dc7611
Navpreet Rajput <84348431+NavpreetRajput@users.noreply.github.com>
2021-06-07 11:53:16 -0400

    Code Cleaning and Formatting

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
-
+// Straight Jasmine testing without Angular's testing support
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
