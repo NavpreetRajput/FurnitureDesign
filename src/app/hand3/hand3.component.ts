614d7c4ef64f5f3bb74520d5339ec102fa66e5bc
Navpreet Rajput <84348431+NavpreetRajput@users.noreply.github.com>
2021-06-10 00:31:46 -0400

    Code Commenting

--- src/app/hand3/hand3.component.ts
+++ src/app/hand3/hand3.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+// this is a declarator which starts with @ sign. The component word marked in bold needs to be the same.
 
 @Component({
   selector: 'app-hand3',
+//   Reference to the html file
   templateUrl: './hand3.component.html',
+//   Reference to the css file
   styleUrls: ['./hand3.component.css']
 })
 export class Hand3Component implements OnInit {
