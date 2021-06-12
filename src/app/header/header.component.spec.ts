f134f1de12939587e20bf785dbfedd4076b53b13
Navpreet Rajput <84348431+NavpreetRajput@users.noreply.github.com>
2021-06-11 20:20:29 -0400

    Code Commenting

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+// Straight Jasmine testing without Angular's testing support
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
