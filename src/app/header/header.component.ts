d8ad8d033cb4240166d42118ae3d8d899d92dcec
Navpreet Rajput <84348431+NavpreetRajput@users.noreply.github.com>
2021-06-11 20:22:29 -0400

    Code Cleaning and Formatting

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// this is a declarator which starts with @ sign. The component word marked in bold needs to be the same.
 
 @Component({
   selector: 'app-header',
