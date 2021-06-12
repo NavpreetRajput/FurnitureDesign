0050e51ac773876c567490f11cb3c515640cb8e5
Navpreet Rajput <84348431+NavpreetRajput@users.noreply.github.com>
2021-06-11 20:21:45 -0400

    Code Cleaning and Formatting

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+//   Reference to the html file
   templateUrl: './header.component.html',
+//   Reference to the css file
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
