51e67e823e796e1f4bc66de7dd0784876ccce74c
Navpreet Rajput <navpreet.k.rajput@gmail.com>
2021-06-14 12:33:13 -0400

    Code Commenting

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+// Straight Jasmine testing without Angular's testing support
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
