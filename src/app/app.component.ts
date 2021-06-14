aabf4ff4c4de212eaf60038148461c2368d6d3d1
Navpreet Rajput <navpreet.k.rajput@gmail.com>
2021-06-14 12:40:36 -0400

    Commented out code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+// this is a declarator which starts with @ sign. The component word marked in bold needs to be the same.
 
 @Component({
   selector: 'app-root',
+//   Reference to the html File
   templateUrl: './app.component.html',
+//   Reference to the css File
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
