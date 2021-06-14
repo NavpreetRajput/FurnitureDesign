a208967df6ad8e78309836f127b76eae5b2a22d0
Navpreet Rajput <navpreet.k.rajput@gmail.com>
2021-06-14 12:34:07 -0400

    Code Cleaning

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+// this is a declarator which starts with @ sign. The component word marked in bold needs to be the same.
 
 @Component({
   selector: 'app-home',
+//   Reference to html file
   templateUrl: './home.component.html',
+ // Reference to css file
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
