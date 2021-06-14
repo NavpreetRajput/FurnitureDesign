2af4149cae362bb1770cb3e4a4131eef971bfd95
Navpreet Rajput <navpreet.k.rajput@gmail.com>
2021-06-14 12:38:59 -0400

    Commented out Code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+// Straight Jasmine testing without Angular's testing support
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
