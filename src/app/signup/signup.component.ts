a2a16988d4503dc10e9b86fdc88f6cc32584e509
Navpreet Rajput <navpreet.k.rajput@gmail.com>
2021-06-19 23:27:53 -0400

    Code Commenting

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+// this is a declarator which starts with @ sign. The component word marked in bold needs to be the same.
 
 @Component({
   selector: 'app-signup',
+ // Reference to the html file
   templateUrl: './signup.component.html',
+//   Reference to the css file
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
