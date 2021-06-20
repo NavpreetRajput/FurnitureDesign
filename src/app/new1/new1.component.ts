2fb9a10aa939f76dfaa8e4fad7f5e2dc1891e1da
Navpreet Rajput <navpreet.k.rajput@gmail.com>
2021-06-19 23:24:31 -0400

    Code Cleaning and Formatting

--- src/app/new1/new1.component.ts
+++ src/app/new1/new1.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+// this is a declarator which starts with @ sign. The component word marked in bold needs to be the same.
 
 @Component({
   selector: 'app-new1',
+//   Reference to the html file
   templateUrl: './new1.component.html',
+//   Reference to the css file
   styleUrls: ['./new1.component.css']
 })
 export class New1Component implements OnInit {
