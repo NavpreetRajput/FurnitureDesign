0c669e6f6b30e623b560378449cba9297ee341d1
Navpreet Rajput <navpreet.k.rajput@gmail.com>
2021-06-19 23:34:52 -0400

    Code Commenting

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import{trigger,state,style,transition,animate} from'@angular/animations';
+// this is a declarator which starts with @ sign. The component word marked in bold needs to be the same.
 
 @Component({
   selector: 'app-sidenav',
+//   Reference to the html file
   templateUrl: './sidenav.component.html',
+//   Reference to the css file.
   styleUrls: ['./sidenav.component.css'],
  
 })
