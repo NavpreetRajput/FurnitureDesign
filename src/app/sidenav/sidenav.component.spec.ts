a020ae919c1d7ffcd24d53dffc789079c803195b
Navpreet Rajput <navpreet.k.rajput@gmail.com>
2021-06-19 23:34:02 -0400

    Code Cleaning and Formatting

--- src/app/sidenav/sidenav.component.spec.ts
+++ src/app/sidenav/sidenav.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidenavComponent } from './sidenav.component';
+// Straight Jasmine testing without Angular's testing support
 
 describe('SidenavComponent', () => {
   let component: SidenavComponent;
